3998e2d4b4018b7b0c5c177da4ffa3fd01b687db
aidanaASC6 <adofire11@gmail.com>
2019-08-09 17:52:39 -0400

    added interview questions

---
+++ interview.js
@@ -0,0 +1,54 @@
+const interviewQuestions = {
+    "questions": [
+        {
+            "field":"computerScience",    
+            "jobTitle":"softwareEngineer", 
+            "question":"You have a 100 coins laying flat on a table, each with a head side and a tail side. 10 of them are heads up, 90 are tails up. You can't feel, see or in any other way find out which side is up. Split the coins into two piles such that there are the same number of heads in each pile.",           
+            "answer":"Place 50 coins into two piles on its edges so that both have the same amount of heads in each pile, neither facing up or down. Answer #2: Trick question, place 50 coins in both piles and in theory they all have heads just not necessarily facing up or down.", 
+        },
+        {
+            "field":"computerScience",    
+            "jobTitle":"softwareEngineer", 
+            "question":"Suppose you are writing a test script to test a function running in a process over night. How do you make sure that if the process is halted, your test function will be able to detect that.",           
+            "answer":"Run the test function in an independent process.", 
+        },
+        {
+            "field":"computerScience",    
+            "jobTitle":"softwareEngineer", 
+            "question":"What sort would you use if you required tight max time bounds and wanted highly regular performance.",           
+            "answer":"Vector sort.", 
+        },
+        {
+            "field":"computerScience",    
+            "jobTitle":"softwareEngineer", 
+            "question":"Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.",            
+        },
+        {
+            "field":"computerScience",    
+            "jobTitle":"softwareEngineer", 
+            "question":"What is the difference between stack and queue?",           
+            "answer":"Queue is always first in, first out. Stack is always last in, first out", 
+        },
+        {
+            "field":"computerScience",    
+            "jobTitle":"softwareEngineer",
+            "question":"Describe the difference between interface-oriented, object-oriented and aspect-oriented programming",           
+            "answer":"Interface programming is contract based. Object-oriented is a way to write granular objects which have a single purpose. Aspect oriented programming is to segregate the code in such a manner that various objects carry the main tasks, and the subsidiary tasks are carried by independent objects.", 
+        },
+        {
+            "field":"computerScience",    
+            "jobTitle":"softwareEngineer", 
+            "question":"Explain the differences between a Thread and a Process?",           
+            "answer":"A process is instance of  the computer program.In a single program it is possible to have one or more threads.", 
+        },
+        {
+            "field":"mechanics",    
+            "jobTitle":"automotiveEngineer", 
+            "question":"",           
+            "answer":"", 
+        },
+        
+    ]
+}
+
+
