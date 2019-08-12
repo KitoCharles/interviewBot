454b644789860716b5224120ee2774739c3775e0
kitoASC6 <kitr2223@outlook.com>
2019-08-12 18:30:44 -0400

    updated index.html

--- interview.js
+++ interview.js
@@ -3,37 +3,37 @@ const interviewQuestions = {
 
         // Software Engineer
         {
-            "field": "computerScience",
+            "field": "engineer",
             "jobTitle": "Software Engineer",
-            "question": "You have 100 coins laying flat on a table, each with a head side and a tail side. 10 of them are heads up, 90 are tails up. You can't feel, see or in any other way find out which side is up. Split the coins into two piles such that there are the same number of heads in each pile. How would you solve this?",
+            "question": "What programming languages have you used in the past? What are your top two programming languages?",
             "answer": "Trick question. Place 50 coins in both piles and in theory they all have heads just not necessarily facing up or down.",
         },
         {
-            "field": "computerScience",
+            "field": "engineer",
             "jobTitle": "Software Engineer",
             "question": "Suppose you are writing a test script to test a function running in a process over night. How do you make sure that if the process is halted, your test function will be able to detect that.",
             "answer": "Run the test function in an independent process.",
         },
         {
-            "field": "computerScience",
+            "field": "engineer",
             "jobTitle": "Software Engineer",
             "question": "What sort would you use if you required tight max time bounds and wanted highly regular performance.",
             "answer": "Vector sort.",
         },
         {
-            "field": "computerScience",
+            "field": "engineer",
             "jobTitle": "Software Engineer",
             "question": "What is the difference between stack and queue?",
             "answer": "Queue is always first in, first out. Stack is always last in, first out",
         },
         {
-            "field": "computerScience",
+            "field": "engineer",
             "jobTitle": "Software Engineer",
             "question": "Describe the difference between interface-oriented, object-oriented and aspect-oriented programming",
             "answer": "Interface programming is contract based. Object-oriented is a way to write granular objects which have a single purpose. Aspect oriented programming is to segregate the code in such a manner that various objects carry the main tasks, and the subsidiary tasks are carried by independent objects.",
         },
         {
-            "field": "computerScience",
+            "field": "engineer",
             "jobTitle": "Software Engineer",
             "question": "Explain the differences between a Thread and a Process?",
             "answer": "A process is instance of  the computer program.In a single program it is possible to have one or more threads.",
@@ -41,47 +41,49 @@ const interviewQuestions = {
 
         // Automotive Engineer
         {
-            "field": "tradesmen",
+            "field": "engineer",
             "jobTitle": "Automotive Engineer",
             "question": "Explain the basic difference between Bs2 And Bs3 engine.",
             "answer": "The basic difference of BS2 and BS3 engines is the presence of a catalytic convertor. The catalytic convertor is present in the BS3 engine which helps in reducing the formation of HC and CO. While in BS2 engine, no catalytic convertor is present which forms HC and CO. Thus in BS3 engine there is no emission of harmful CO and HC. The full form of BS is bharat stage, which is a standard of emission in India.",
         },
         {
-            "field": "tradesmen",
+            "field": "engineer",
             "jobTitle": "Automotive Engineer",
             "question": "In a tractor, explain how the hydrostatic propulsion systems work?",
             "answer": "This system is based on Pascal’s law. In a tractor the pressure is same, so the force given by the liquid to the surrounding is equal to the pressure X area. Thus, obeying the Pascal’s law hydrostatic propulsion system is designed, similarly small piston feels small force and the large piston feels larger area.",
         },
         {
-            "field": "tradesmen",
+            "field": "engineer",
             "jobTitle": "Automotive Engineer",
             "question": "How does humidity affect cooling for a radiator? A person?",
             "answer": "Radiator cools more efficiently during high-humidity because it does not rely on evaporative cooling. A person cools less efficiently because the body relies on evaporative cooling and when the air high a high umidity content evaporation occurs slowly.",
         },
         {
-            "field": "tradesmen",
+            "field": "engineer",
             "jobTitle": "Automotive Engineer",
             "question": "Explain how you can keep your car in good condition?",
             "answer": "Regular check-up of parts  like the battery, tires and brakes fluid levels ( coolant, washer fluid, brake fluid, oil, power steering, etc.) fuel filters and injector belts and hose alternator lubrication of wheel bearing and ball joints",
         },
         {
-            "field": "tradesmen",
+            "field": "engineer",
             "jobTitle": "Automotive Engineer",
             "question": "How often does a car requires services?",
             "answer": "Car requires services yearly and major maintenance at 6,000 miles.",
         },
         {
-            "field": "tradesmen",
+            "field": "engineer",
             "jobTitle": "Automotive Engineer",
             "question": "Before buying a used car, what things you should look for?",
             "answer": "Any leakage of oil underneath the car. Check any leakage of coolant water from the radiator. Check battery condition. Check whether car emits blue gray, dense smoke or a normal smoke from exhaust pipe. Watch out for engine noises, any vibrations, lack of power or any other issues. Check for any burning smells when open the hood. Check steering wheel, driver’s seat and inner door. Check tires ( Edge should not worn out, no bulge on the sidewall, no cuts, enough groves to hold the road). Check all meter work fine when you start the car",
         },
         {
-            "field": "tradesmen",
+            "field": "engineer",
             "jobTitle": "Automotive Engineer",
             "question": "Explain how timing belt works?",
             "answer": "Timing belt rests on the crankshaft pulley and either one or two camshaft pulley; they are all align through timing belt. The closing and opening of engine valves are controlled by camshaft to let in air and fuel mixture or exhaust gas, if you do not have a timing belt or damaged timing,  it will directly affect the timing of opening and closing of engine valves. This is the reason it is called timing belt and if not in a good position, it will damage your engine.",
         },
+
+        // Plumber
         {
             "field": "tradesmen",
             "jobTitle": "Plumber",
