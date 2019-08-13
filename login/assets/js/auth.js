113ea36d69cb454f6e5645a4fc36721b8ab7ec02
arASC6 <hiers256@gmail.com>
2019-08-13 02:20:43 -0400

    functional Logout/SignIn minor bugs

---
+++ login/assets/js/auth.js
@@ -0,0 +1,63 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDzdK2IW_uY-JTPCZ-ELAFGx8IOOYdVANM",
+    authDomain: "interviewbot-f6587.firebaseapp.com",
+    databaseURL: "https://interviewbot-f6587.firebaseio.com",
+    projectId: "interviewbot-f6587",
+    storageBucket: "interviewbot-f6587.appspot.com",
+    messagingSenderId: "323622453220",
+    appId: "1:323622453220:web:f20d41d9e3201076"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  //Gain Access to firebase
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+
+//get elements
+const txtEmail = document.getElementById('emailtTxt')
+const txtPassword = document.getElementById('passwordTxt')
+const loginForm = document.querySelector('#signin-form')
+const logoutForm = document.querySelector('#signOutBtn')
+
+
+
+
+//sign in
+ loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+
+    auth.signInWithEmailAndPassword(email, pass).then(cred => {
+      console.log(cred.user)
+    });
+
+ });
+
+//sign out
+
+logoutForm.addEventListener('click', (e) => {
+  e.preventDefault();
+
+
+  auth.signOut()
+    
+  
+
+});
+
+
+
+//checks if user is logged in or not and does things accordingly
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser){
+      console.log("logged in")
+      //want this to go to profile homepage
+  }else {
+      console.log('not logged in')
+  }
+});
+
+
